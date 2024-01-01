d21b67f52ddfcf51a56f8609908a045cc11051a7
Syed Mustafa Hassan <fa21bscs0030@maju.edu.pk>
2024-01-02 00:11:19 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	char name[20];
+	
+	cout<<"Enetr your name : ";
+	cin>>name;
+	
+	cout<<"\nYour name is "<<name;	
+	
+	return 0;
+}
